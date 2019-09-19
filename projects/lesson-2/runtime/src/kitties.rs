49119fdf71ad8696e7951982ef24ae53473b6509
marco_mac <sun.dsk1@icloud.com>
2019-09-19 09:23:06 +0800

    lesson 2

--- projects/lesson-2/runtime/src/kitties.rs
+++ projects/lesson-2/runtime/src/kitties.rs
@@ -8,25 +8,45 @@
 /// For more guidance on Substrate modules, see the example module
 /// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
 
+use codec::{Encode, Decode};
 use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
 use system::ensure_signed;
+use sr_primitives::traits::Hash;
 
-/// The module's configuration trait.
-pub trait Trait: system::Trait {
-	// TODO: Add other types and constants required configure this module.
+// NOTE: We have added this struct template for you
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Kitty<Hash, Balance> {
+    // ACTION: Define the properties of your kitty struct here
+	id:		Hash,
+	dna:	u128,
+	price:	Balance,
+	gen:	u64,
+}
 
+
+// /// The module's configuration trait.
+// pub trait Trait: system::Trait {
+// 	// TODO: Add other types and constants required configure this module.
+
+// 	/// The overarching event type.
+// 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+// }
+
+pub trait Trait: balances::Trait {
 	/// The overarching event type.
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 // This module's storage items.
 decl_storage! {
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(something): Option<u32>;
-	}
+	trait Store for Module<T: Trait> as KittyStorage {
+		Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+        OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
+
+        Nonce: u64;
+    }
 }
 
 // The module's dispatchable functions.
@@ -37,21 +57,31 @@ decl_module! {
 		// this is needed only if you are using events in your module
 		fn deposit_event() = default;
 
-		// Just a dummy entry point.
-		// function that can be called by the external world as an extrinsics call
-		// takes a parameter of the type `AccountId`, stores it and emits an event
-		pub fn do_something(origin, something: u32) -> Result {
-			// TODO: You only need this if you want to check it was signed.
-			let who = ensure_signed(origin)?;
+		// create kitty
+		fn create_kitty(origin) -> Result {
+            let sender = ensure_signed(origin)?;
 
-			// TODO: Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+            let nonce = Nonce::get();
+            let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
+                .using_encoded(<T as system::Trait>::Hashing::hash);
 
-			// here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			Ok(())
-		}
+            // ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
+
+            // let new_kitty = Kitty {
+            //     id: random_hash,
+            //     dna: random_hash,
+            //     // price: <T::Balance as As<u64>>::sa(0),
+            //     gen: 0,
+            // };
+
+            // <Kitties<T>>::insert(random_hash, new_kitty);
+            // <KittyOwner<T>>::insert(random_hash, &sender);
+            // <OwnedKitty<T>>::insert(&sender, random_hash);
+
+            // <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
 	}
 }
 
