9f3b9bf4885b306a8361454322fe52ccb29b1198
Alex Wang <idea.wangtj@gmail.com>
2019-09-23 21:45:10 +0800

    homework commit for lesson 3

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -1,4 +1,4 @@
-use support::{decl_module, decl_storage, StorageValue, StorageMap};
+use support::{decl_module, decl_storage, StorageValue, StorageMap, ensure};
 use codec::{Encode, Decode};
 use runtime_io::blake2_128;
 use system::ensure_signed;
@@ -33,5 +33,30 @@ decl_module! {
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
 		}
+
+		/// breed new kitty from father kitty and mother kitty
+		pub fn breed(origin, father_kitty_id: u32, mother_kitty_id: u32) {
+			let sender = ensure_signed(origin)?;
+			let count = Self::kitties_count();
+			if count == u32::max_value() {
+				return Err("Kitties count overflow");
+			}
+
+			//ensure current two kitties as father and mother
+			//ensure!(<Kitties>::exists(father_kitty_id), "father kitty invalid");
+			//ensure!(<Kitties>::exists(mother_kitty_id), "mother kitty invalid");
+
+			// child kitty inherent dna from father and mother kitties
+			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number(),
+			father_kitty_id,
+			mother_kitty_id);
+
+			let dna = payload.using_encoded(blake2_128);
+			let kitty = Kitty(dna);
+			Kitties::insert(count, kitty);
+			KittiesCount::put(count + 1);
+		}
+
+
 	}
 }
