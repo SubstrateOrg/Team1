fc6640d489f40a3ec52f0d665203697bb8462c3a
yang yu <yundkyy@qq.com>
2019-09-23 11:58:34 +0800

    两个bread函数

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -17,18 +17,65 @@ decl_storage! {
 		pub KittiesCount get(kitties_count): u32;
 	}
 }
-
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		/// Create a new kitty
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 			let count = Self::kitties_count();
-			if count == u32::max_value() {
+			if count + 1 == u32::max_value() {
 				return Err("Kitties count overflow");
 			}
 			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 			let dna = payload.using_encoded(blake2_128);
+			let kitty = Kitty(dna);
+			Kitties::insert(count, kitty);
+			KittiesCount::put(count + 1);
+		}
+		pub fn breed1(origin,k1:u32 ,k2:u32){
+			let sender = ensure_signed(origin)?;
+			let count = Self::kitties_count();
+			if count + 1 == u32::max_value() {
+				return Err("Kitties count overflow");
+			}
+			let kitty1 = Self::kitty(k1);
+			let kitty2 = Self::kitty(k2);
+			let payload = (kitty1.0,kitty2.0, <system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let dna = payload.using_encoded(blake2_128);
+			let kitty = Kitty(dna);
+			Kitties::insert(count, kitty);
+			KittiesCount::put(count + 1);
+		}
+		pub fn breed2(origin,k1:u32 ,k2:u32){
+			let sender = ensure_signed(origin)?;
+			let count = Self::kitties_count();
+			if count + 1 == u32::max_value() {
+				return Err("Kitties count overflow");
+			}
+			let kitty1 = Self::kitty(k1);
+			let kitty2 = Self::kitty(k2);
+			let payload = (kitty1.0,kitty2.0, <system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let mut dna = payload.using_encoded(blake2_128);
+			let mut i = 0;
+			let dna1 = kitty1.0;
+			let dna2 = kitty2.0;
+			while i<16 {
+				match dna[i] % 3  {
+					1 => {
+						dna[i] = dna1[i] + dna2[i];
+					},
+					2 => {
+						dna[i] = dna1[i];
+					},
+					0 => {
+						dna[i] = dna2[i];
+					},
+					_ => {
+						dna[i] = 0;
+					}
+				}
+				i = i + 1
+			}
 			let kitty = Kitty(dna);
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
