8eaabfbff8ace518a30027263bb530c7561fb665
marco_mac <sun.dsk1@icloud.com>
2019-09-26 10:01:42 +0800

    homework4 12

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -33,25 +33,8 @@ decl_module! {
 			let sender = ensure_signed(origin)?;
 
 			// 作业：重构create方法，避免重复代码
+			Self::insert_kitty(sender.clone(), Self::next_kitty_id()?, Kitty(Self::random_value(&sender)));
 
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
-
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
 		}
 
 		/// Breed kitties
@@ -69,7 +52,15 @@ fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
 	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
 	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
 	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	let mut res = 0b00000000;
+	for idx in [1, 2, 4, 8, 16, 32, 64, 128].iter() {
+		if selector&idx != 0 {
+			res = res|(dna1&idx);
+		} else {
+			res = res|(dna2&idx);
+		}
+	}
+	return res;
 }
 
 impl<T: Trait> Module<T> {
