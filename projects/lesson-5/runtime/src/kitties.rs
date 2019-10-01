16f7ff4ff69b28147e1b7500631dd39de0b9f711
marco_mac <sun.dsk1@icloud.com>
2019-10-01 16:54:19 +0800

    homework 5

--- projects/lesson-5/runtime/src/kitties.rs
+++ projects/lesson-5/runtime/src/kitties.rs
@@ -55,6 +55,16 @@ decl_module! {
 		// 作业：实现 transfer(origin, to: T::AccountId, kitty_id: T::KittyIndex)
 		// 使用 ensure! 来保证只有主人才有权限调用 transfer
 		// 使用 OwnedKitties::append 和 OwnedKitties::remove 来修改小猫的主人
+		pub fn transfer(origin, to: T::AccountId, kitty_id: T::KittyIndex) {
+			let sender = ensure_signed(origin)?;
+			// let have = Self::owned_kitties((sender, Some(kitty_id)));
+			ensure!( Self::owned_kitties((sender.clone(), Some(kitty_id))) != None, "U do NOT own this kitty!");
+			
+			<OwnedKitties<T>>::remove(&sender, kitty_id);
+			
+			<OwnedKitties<T>>::append(&to, kitty_id);
+
+		}
 	}
 }
 
@@ -142,6 +152,12 @@ impl<T: Trait> Module<T> {
 
 	fn insert_owned_kitty(owner: &T::AccountId, kitty_id: T::KittyIndex) {
 		// 作业：调用 OwnedKitties::append 完成实现
+		// if <OwnedKitties<T>>::get(&(owner.clone(), Some(kitty_id))) == None {
+		// 	<OwnedKitties<T>>::append(owner, kitty_id);
+		// }
+		if Self::owned_kitties((owner.clone(), Some(kitty_id))) == None {
+			<OwnedKitties<T>>::append(owner, kitty_id);
+		}
   	}
 
 	fn insert_kitty(owner: &T::AccountId, kitty_id: T::KittyIndex, kitty: Kitty) {
